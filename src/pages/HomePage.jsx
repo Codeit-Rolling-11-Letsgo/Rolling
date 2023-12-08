b1bc8347f4871dc3258074b81094a180c5cf1692
dkile <dkile532@naver.com>
2023-12-08 23:41:28 +0900

    vscode 모듈 자동 추가 시 절대 경로로 설정

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,12 @@
 import styles from '@/App.module.scss';
+import { PATH_HOME } from '@/constants/routes';
+
+// import { PATH_HOME } from '../constants/routes';
 
 export default function HomePage() {
-	return <div className={styles.text_28}>홈페이지</div>;
+	return (
+		<a href={PATH_HOME} className={styles.text_28}>
+			홈페이지
+		</a>
+	);
 }
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), eslintPlugin()],
+	plugins: [react()],
 	resolve: {
 		alias: {
 			'@': resolve(__dirname, './src'),

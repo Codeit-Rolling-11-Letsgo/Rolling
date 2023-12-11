addf73f6861f79a6289ee15bb8831927897a658e
dkile <dkile532@naver.com>
2023-12-11 17:36:36 +0900

    Feat: 홈페이지 레이아웃 구현

---
+++ src/components/home/HomeLayout.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+
+import Layout from '@/components/common/Layout';
+import styles from '@/components/home/HomeLayout.module.scss';
+import { PATH_POST } from '@/constants/routes';
+
+export default function HomeLayout({ children }) {
+	return (
+		<Layout addon={<HomeHeaderCTA />}>
+			<div className={styles.container}>{children}</div>
+		</Layout>
+	);
+}
+
+function HomeHeaderCTA() {
+	return (
+		<Link to={PATH_POST} className={styles.cta}>
+			롤링 페이퍼 만들기
+		</Link>
+	);
+}

2e2d9a8bd1879f9cc01b5cf4e2a717afb2991761
So-Yeon Ahn <sozygn@gmail.com>
2023-12-11 09:56:41 +0900

    fix: hover시 글자색 변경 적용

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from '@/pages/home/HomePage';
 import ListPage from '@/pages/list/ListPage';
 import PostPage from '@/pages/post/PostPage';
+import DropDown from '@/components/common/DropDown';
 
 const router = createBrowserRouter([
 	{
@@ -25,6 +26,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		{/* <RouterProvider router={router} /> */}
+		<DropDown />
 	</React.StrictMode>,
 );

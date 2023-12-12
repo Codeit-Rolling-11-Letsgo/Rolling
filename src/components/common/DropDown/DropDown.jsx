9e2684df4a68f1aa192fe220516cd0e162bdfed8
안소연 <148179726+sozign@users.noreply.github.com>
2023-12-12 20:21:59 +0900

    feat: DropDown 컴포넌트 제작 (#15)
    * feat: DropDown 컴포넌트 제작
    
    * feat: disabled, error 경우 대응
    
    * fix: hover시 글자색 변경 적용
    
    * fix: DropDown 폴더 생성
    
    * docs: js doc 작성
    
    * fix: 리뷰반영 (커밋 메세지 본문참조)
    
    1. 파이어폭스, 사파리에서 일부 스타일이 적용되지 않는 오류 해결
    2. onChange prop 추가
    3. 일반/focus/active/hover 스타일 조정
    4. focus시 border 두꺼워지며 컴포넌트 밀리는 현상 해결
    
    * fix: 'disabled' prop default value 수정, 스타일 수정
    
    * fix: 옵션 선택시 드롭다운 목록 닫히게
    
    * fix: onChange 이벤트 발생하지 않던 오류 해결, state명 수정
    
    * docs: dropDown js doc 수정
    
    * refactor: clsx 사용, className 정돈
    
    * refactor: disabled 상태 가상 클래스로 스타일링
    
    * fix: 길이가 긴 옵션이 선택되었을 경우 옵션 텍스트가 컴포넌트 밖으로 나오는 현상 해결
    
    * refactor: 리뷰사항 반영
    
    * refactor: onChange 함수를 handleOptionSelect에 위치시킴
    
    ---------
    
    Co-authored-by: So-Yeon Ahn <sozygn@gmail.com>

---
+++ src/components/common/DropDown/DropDown.jsx
@@ -0,0 +1,90 @@
+import clsx from 'clsx';
+import { useState } from 'react';
+
+import styles from '@/components/common//DropDown/DropDown.module.scss';
+import Icon from '@/components/common/icon/Icon';
+
+/**
+ * 옵션을 선택할 수 있는 드롭다운 컴포넌트입니다.
+ *
+ * @component
+ * @example
+ * // 기본 사용법:
+ * <DropDown options={['옵션 1', '옵션 2', '옵션 3']} defaultValue="기본 옵션으로 선택할 값" onChange={값이 선택될 때 실행할 콜백}/>
+ * onChange 콜백의 파라미터로 사용자가 선택한 옵션의 값이 string으로 전달됩니다. (line 38)
+ * onChange 콜백은 본 컴포넌트의 필수 옵션이므로 prop으로 전달해주지 않으면 에러가 발생합니다. 사용시 유의해주세요.
+ *
+ * // 에러 메시지가 있는 경우:
+ * <DropDown options={['옵션 1', '옵션 2', '옵션 3']} errorMessage="에러 발생 알림" onChange={값이 선택될 때 실행할 콜백} />
+ * @param {{options?: []string}} props
+ * @param {{defaultValue?: string}} props
+ * @param {{onChange: object}} props
+ * @param {{errorMessage: string}} props
+ * @param {{disabled?: boolean}} props
+ */
+
+export default function DropDown({
+	options = ['옵션1', '옵션2', '옵션3'],
+	defaultValue = 'default value',
+	onChange,
+	errorMessage = '',
+	className,
+	disabled = false,
+	...props
+}) {
+	const [isOpened, setIsOpened] = useState(false);
+	const [selectedOption, setSelectedOption] = useState(defaultValue);
+
+	const handleOpenDropDown = () => {
+		setIsOpened(!isOpened);
+	};
+
+	const handleOptionSelect = ({ target }) => {
+		onChange(target.value);
+		setSelectedOption(target.value);
+		setIsOpened(!isOpened);
+	};
+
+	return (
+		<div className={className} {...props}>
+			<button
+				type='button'
+				disabled={disabled || errorMessage}
+				className={clsx(
+					styles.selectContainer,
+					disabled && styles.disabled,
+					errorMessage && styles.error,
+				)}
+				onClick={handleOpenDropDown}
+			>
+				<input
+					disabled={disabled}
+					type='button'
+					value={selectedOption}
+					className={clsx(styles.selectedOption, disabled && styles.disabled)}
+				/>
+				<Icon
+					name='arrowDown'
+					className={clsx(styles.Icon, isOpened && styles.rotate)}
+				/>
+			</button>
+			{errorMessage && (
+				<div className={styles.errorMessage}>{errorMessage}</div>
+			)}
+			{isOpened && (
+				<div className={styles.options}>
+					{options.map((option) => (
+						<input
+							type='button'
+							disabled={disabled}
+							key={option.slice(-1)}
+							onClick={handleOptionSelect}
+							className={styles.optionItem}
+							value={option}
+						></input>
+					))}
+				</div>
+			)}
+		</div>
+	);
+}

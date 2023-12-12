d0760b61c2894f5ccbe649d4c7321158a4c4316c
So-Yeon Ahn <sozygn@gmail.com>
2023-12-12 09:42:14 +0900

    docs: dropDown js doc 수정

--- src/components/common/DropDown/DropDown.jsx
+++ src/components/common/DropDown/DropDown.jsx
@@ -9,10 +9,17 @@ import Icon from '@/components/common/icon/Icon';
  * @component
  * @example
  * // 기본 사용법:
- * <DropDown options={['옵션 1', '옵션 2', '옵션 3']} defaultValue="옵션 선택" />
+ * <DropDown options={['옵션 1', '옵션 2', '옵션 3']} defaultValue="기본 옵션으로 선택할 값" onChange={값이 선택될 때 실행할 콜백}/>
+ * onChange 콜백의 파라미터로 사용자가 선택한 옵션의 값이 string으로 전달됩니다. (line 38)
+ * onChange 콜백은 본 컴포넌트의 필수 옵션이므로 prop으로 전달해주지 않으면 에러가 발생합니다. 사용시 유의해주세요.
  *
  * // 에러 메시지가 있는 경우:
- * <DropDown options={['옵션 1', '옵션 2', '옵션 3']} errorMessage="옵션을 선택해주세요" />
+ * <DropDown options={['옵션 1', '옵션 2', '옵션 3']} errorMessage="에러 발생 알림" onChange={값이 선택될 때 실행할 콜백} />
+ * @param {{options?: []string}} props
+ * @param {{defaultValue?: string}} props
+ * @param {{onChange: object}} props
+ * @param {{errorMessage: string}} props
+ * @param {{disabled?: boolean}} props
  */
 
 export default function DropDown({

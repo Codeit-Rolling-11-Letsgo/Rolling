cf6f9fc7e0ea9dc473c4ed206279b62cfa90a550
ckswnskfk <ckswnskfk@hanmail.net>
2023-12-12 15:11:28 +0900

    Feat: PostLayout에 이모지 바(pageTitle) prop 추가

--- src/pages/post/PostLayout.jsx
+++ src/pages/post/PostLayout.jsx
@@ -1,9 +1,10 @@
 import Layout from '@/components/common/Layout';
 import styles from '@/pages/post/PostLayout.module.scss';
 
-function PostLayout({ children }) {
+function PostLayout({ children, pageTitle }) {
 	return (
 		<Layout>
+			{pageTitle}
 			<div className={styles.container}>{children}</div>
 		</Layout>
 	);

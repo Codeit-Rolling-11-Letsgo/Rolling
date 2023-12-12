6c5e87a43b8585636634858f1a668a54ab57a8e4
d0ngbb00 <lauren3021504@gmail.com>
2023-12-12 16:42:44 +0900

    Refactor: outlined버튼 추가 및 리뷰 반영

---
+++ src/components/common/Buttons/Button.jsx
@@ -0,0 +1,32 @@
+import '@/components/common/Buttons/Button.scss';
+
+import Icon from '@/components/common/icon/Icon';
+
+function Button({ variant, size, disabled, content, ...props }) {
+	let buttonClass = 'button';
+
+	if (size) {
+		buttonClass += ` ${size}`;
+	}
+
+	if (variant) {
+		buttonClass += ` ${variant}`;
+	}
+
+	if (variant === 'iconOutlined') {
+		return (
+			<button className={buttonClass} {...props} disabled={disabled}>
+				<Icon type='svg' name='add' className='icon' />
+				{content}
+			</button>
+		);
+	}
+
+	return (
+		<button className={buttonClass} {...props} disabled={disabled}>
+			{content}
+		</button>
+	);
+}
+
+export default Button;

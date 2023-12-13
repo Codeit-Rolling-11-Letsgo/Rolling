66db538fdaa47a91587e82f646c5d6e85528a904
안소연 <148179726+sozign@users.noreply.github.com>
2023-12-13 18:40:57 +0900

    feat: EmojiPickerButton 컴포넌트 제작 (#27)
    * feat: EmojiPickerButton 컴포넌트 제작
    
    * review: 이모지 클릭시 토글 닫히는 기능 추가
    
    ---------
    
    Co-authored-by: So-Yeon Ahn <sozygn@gmail.com>

---
+++ src/components/HeaderService/EmojiPickerButton.jsx
@@ -0,0 +1,54 @@
+import EmojiPicker from 'emoji-picker-react';
+import { useEffect, useRef, useState } from 'react';
+
+import styles from './EmojiPickerButton.module.scss';
+
+const EMOJI_PICKER_WIDTH = 307; // library 특성상 px로 조정
+const EMOJI_PICKER_HEIGHT = 393; // library 특성상 px로 조정
+
+export default function EmojiPickerButton({ onEmojiClick }) {
+	// onEmojiClick 핸들러에 파라미터로 전달되는 객체
+	//activeSkinTone: "neutral" emoji: "😀" getImageUrl: ƒ getImageUrl(emojiStyle)imageUrl: "https://cdn.jsdelivr.net/npm/emoji-datasource-apple/img/apple/64/1f600.png" isCustom: false names: (2) ['grinning', 'grinning face'] unified: "1f600" unifiedWithoutSkinTone: "1f600"
+	const [isPickerOpened, setIsPickerOpened] = useState(false);
+	const emojiPickerRef = useRef(null);
+
+	const handleEmojiClick = (emojiInfo) => {
+		setIsPickerOpened(false);
+		onEmojiClick(emojiInfo);
+	};
+
+	const handlePickerToggle = (e) => {
+		e.stopPropagation(); //handleOutsideClick 이벤트 발생 방지
+		setIsPickerOpened(!isPickerOpened);
+	};
+
+	const handleOutsideClick = (e) => {
+		if (emojiPickerRef.current && !emojiPickerRef.current.contains(e.target)) {
+			setIsPickerOpened(false);
+		}
+	};
+
+	useEffect(() => {
+		document.addEventListener('click', handleOutsideClick);
+		return () => {
+			document.removeEventListener('click', handleOutsideClick);
+		};
+	}, []);
+
+	return (
+		<div className={styles.emojiPickerComponent}>
+			<button className={styles.toggleButton} onClick={handlePickerToggle}>
+				이모티콘 추가
+			</button>
+			{isPickerOpened && (
+				<div ref={emojiPickerRef}>
+					<EmojiPicker
+						onEmojiClick={handleEmojiClick}
+						width={EMOJI_PICKER_WIDTH}
+						height={EMOJI_PICKER_HEIGHT}
+					/>
+				</div>
+			)}
+		</div>
+	);
+}

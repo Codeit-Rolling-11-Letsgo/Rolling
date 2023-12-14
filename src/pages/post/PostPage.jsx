07dd6cbb6ecc464be900839beeac9d4525bba209
ckswnskfk <ckswnskfk@hanmail.net>
2023-12-15 00:34:27 +0900

    Feat: post 페이지 무한 스크롤 구현

--- src/pages/post/PostPage.jsx
+++ src/pages/post/PostPage.jsx
@@ -1,21 +1,54 @@
+import { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
+import { getMessages } from '@/apis/post/postAPI';
 import NewMessageCTA from '@/components/post/NewMessageCTA';
 import PostCard from '@/components/post/PostCard';
 import PostLayout from '@/components/post/PostLayout';
+import { useIntersect } from '@/hooks/useIntersect';
 import styles from '@/pages/post/PostPage.module.scss';
 
 export default function PostPage() {
-	const { recipientId, response } = useLoaderData();
-	const { results: messages } = response;
+	const { recipientId, messagesInfo } = useLoaderData();
+	const { results: messages } = messagesInfo;
+	const [currentMessages, setCurrentMessages] = useState(messages);
+	const [isLoading, setIsLoading] = useState(false);
+
+	const fetchMoreMessages = async () => {
+		if (messagesInfo.count > currentMessages.length) {
+			setIsLoading(true);
+			const params = new URLSearchParams(messagesInfo.next);
+			const offset = params.get('offset');
+			const { messagesInfo: newMessagesInfo } = await getMessages({
+				recipientId,
+				limit: 5,
+				offset,
+			});
+
+			setCurrentMessages((prevMessages) => [
+				...prevMessages,
+				...newMessagesInfo.results,
+			]);
+			setIsLoading(false);
+		}
+	};
+
+	const intersectionOptions = {
+		rootMargin: '350px',
+		threshold: 0,
+	};
+
+	const trigger = useIntersect(fetchMoreMessages, intersectionOptions);
 
 	return (
 		<PostLayout>
 			<div className={styles.cardList}>
 				<NewMessageCTA recipientId={recipientId} />
-				{messages.map((result) => (
-					<PostCard key={result.id} message={result}></PostCard>
+				{currentMessages.map((message) => (
+					<PostCard key={message.id} message={message}></PostCard>
 				))}
+
+				{!isLoading && <div ref={trigger}></div>}
 			</div>
 		</PostLayout>
 	);

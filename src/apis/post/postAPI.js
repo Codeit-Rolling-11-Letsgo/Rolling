76802f8a4b4cb61d4cc0412e9965ceb3c3829c12
ckswnskfk <ckswnskfk@hanmail.net>
2023-12-15 00:35:23 +0900

    Refactor: 변수명 변경 및 jsdoc 추가

--- src/apis/post/postAPI.js
+++ src/apis/post/postAPI.js
@@ -1,11 +1,24 @@
 import { fetcher } from '@/apis/fetcher';
 
+/**
+ *
+ * @param {{recipientId: number, limit?: number, offset?: number}} param
+ * @returns {Promise<{
+ *  recipientId: number,
+ *  messagesInfo: {
+ *   count: number, // 전체 메세지 수
+ *   next: string,  // 다음에 호출해야 할 url
+ *   previous: string,  // 이전에 호출한 url
+ *   results: Object[]  // 불러온 메세지
+ *  }
+ * }>}
+ */
 export const getMessages = async ({ recipientId, limit, offset }) => {
-	const response = await fetcher
+	const messagesInfo = await fetcher
 		.get(`recipients/${recipientId}/messages/`, {
 			searchParams: { limit, offset },
 		})
 		.json();
 
-	return { recipientId, response };
+	return { recipientId, messagesInfo };
 };

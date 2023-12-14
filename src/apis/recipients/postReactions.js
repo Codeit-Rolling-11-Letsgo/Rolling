bfff193bdb4535d6cc87bf4f2549350947f22e2a
So-Yeon Ahn <sozygn@gmail.com>
2023-12-14 14:05:51 +0900

    feat: 에러 핸들링

--- src/apis/recipients/postReactions.js
+++ src/apis/recipients/postReactions.js
@@ -1,10 +1,14 @@
 import { fetcher } from '@/apis/fetcher';
 
 const postReactions = async (emoji, USER_ID = 962) => {
-	const result = await fetcher.post(`recipients/${USER_ID}/reactions/`, {
-		json: { emoji, type: 'increase' },
-	});
-	return result;
+	try {
+		const result = await fetcher.post(`recipients/${USER_ID}/reactions/`, {
+			json: { emoji, type: 'increase' },
+		});
+		return result;
+	} catch (error) {
+		throw new Error(error);
+	}
 };
 
 export default postReactions;

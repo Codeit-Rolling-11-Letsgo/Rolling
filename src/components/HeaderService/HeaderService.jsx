410d71120f1545d342dcd84b5af319f18259c8b2
So-Yeon Ahn <sozygn@gmail.com>
2023-12-15 10:38:41 +0900

    review: 태그 및 클래스명 수정

--- src/components/HeaderService/HeaderService.jsx
+++ src/components/HeaderService/HeaderService.jsx
@@ -4,8 +4,8 @@ export default function HeaderService() {
 	//api 요청 여기서 할 예정
 	return (
 		<div className={styles.layout}>
-			<div className={styles.toSomeone}>To.~~</div>
-			<div className={styles.reactionByEmoji}>반응 모음</div>
+			<h2 className={styles.title}>To.~~</h2>
+			<div className={styles.service}>반응 모음</div>
 		</div>
 	);
 }

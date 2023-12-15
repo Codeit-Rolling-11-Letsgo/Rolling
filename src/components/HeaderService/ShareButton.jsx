166fb9b5acf698e5f8586e2639720d9093f03bfe
안소연 <148179726+sozign@users.noreply.github.com>
2023-12-15 11:48:17 +0900

    feat: 이모지바 url공유 기능 제작 (#36)
    * feat: url공유 기능 제작
    
    * review: 리뷰사항 반영
    
    * review: 토스트 위치 변경
    
    ---------
    
    Co-authored-by: So-Yeon Ahn <sozygn@gmail.com>

---
+++ src/components/HeaderService/ShareButton.jsx
@@ -0,0 +1,84 @@
+import clsx from 'clsx';
+import { useEffect, useRef, useState } from 'react';
+
+import Icon from '@/components/common/icon/Icon';
+
+import styles from './ShareButton.module.scss';
+
+export default function ShareButton() {
+	const [isPickerOpened, setIsPickerOpened] = useState(false);
+	const [isToastVisible, setIsToastVisible] = useState(false);
+	const shareOptionPickerRef = useRef(null);
+
+	const handlePickerToggle = (e) => {
+		e.stopPropagation();
+		setIsPickerOpened(!isPickerOpened);
+	};
+
+	const handleOutsideClick = (e) => {
+		if (shareOptionPickerRef.current !== e.target) {
+			setIsPickerOpened(false);
+		}
+	};
+
+	const handleCopyUrl = (e) => {
+		const currentUrl = window.location.href;
+		navigator.clipboard
+			.writeText(currentUrl)
+			.then(() => {
+				setIsToastVisible(true);
+				setTimeout(() => {
+					setIsToastVisible(false);
+				}, 5000);
+			})
+			.catch((error) => {
+				throw new error(error);
+			});
+
+		handlePickerToggle(e);
+	};
+
+	useEffect(() => {
+		document.addEventListener('click', handleOutsideClick);
+		return () => {
+			document.removeEventListener('click', handleOutsideClick);
+		};
+	}, []);
+
+	return (
+		<div className={styles.shareOption}>
+			<button className={styles.toggleButton} onClick={handlePickerToggle}>
+				공유
+			</button>
+			{isPickerOpened && (
+				<div className={styles.shareOptionList} ref={shareOptionPickerRef}>
+					<button
+						onClick={handlePickerToggle}
+						className={styles.shareOptionItem}
+					>
+						카카오톡 공유
+					</button>
+					<button onClick={handleCopyUrl} className={styles.shareOptionItem}>
+						URL 공유
+					</button>
+				</div>
+			)}
+			{isToastVisible && (
+				<div className={clsx(styles.toast)}>
+					<div className={styles.toastContents}>
+						<Icon name='complete' />
+						<div className={styles.toastText}>URL이 복사되었습니다</div>
+					</div>
+					<button
+						className={styles.toastClose}
+						onClick={() => {
+							setIsToastVisible(false);
+						}}
+					>
+						<Icon className={styles.toastIcon} name='close' />
+					</button>
+				</div>
+			)}
+		</div>
+	);
+}

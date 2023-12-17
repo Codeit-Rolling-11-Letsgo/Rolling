8b1762a8a019a97d8f3337441d2507dd62669450
ckswnskfk <ckswnskfk@hanmail.net>
2023-12-17 18:54:31 +0900

    Refactor: 사용하지 않는 파일 삭제

--- src/utils/commonUtils.js
+++
@@ -1,6 +0,0 @@
-export const getLimitByResolution = () => {
-	if (matchMedia('(max-width: 1248px) and (min-width: 768px)').matches)
-		return 8;
-	if (matchMedia('(max-width: 767px)').matches) return 5;
-	return 3;
-};

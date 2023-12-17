2b5ee92d4b0cb9de6ec4ea719a1e658bf62002a3
dkile <dkile532@naver.com>
2023-12-17 19:19:51 +0900

    Chore: section 태그에 aria label 추가

--- src/components/list/ListContent.jsx
+++ src/components/list/ListContent.jsx
@@ -2,9 +2,11 @@ import styles from '@/components/list/ListContent.module.scss';
 
 export default function ListContet({ title, addon, children }) {
 	return (
-		<section>
+		<section role='contentinfo' aria-labelledby='title'>
 			<header className={styles.header}>
-				<h2 className={styles.title}>{title}</h2>
+				<h2 id='title' className={styles.title}>
+					{title}
+				</h2>
 				<div>{addon}</div>
 			</header>
 			<div className={styles.content}>{children}</div>

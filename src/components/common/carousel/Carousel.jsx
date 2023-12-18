743997f62e35a3e1cd19bafbefbeb08ab326c633
dkile <dkile532@naver.com>
2023-12-18 16:59:52 +0900

    Feat: 캐러셀 구현 (#52)

---
+++ src/components/common/carousel/Carousel.jsx
@@ -0,0 +1,91 @@
+import clsx from 'clsx';
+import { useRef, useState } from 'react';
+
+import styles from '@/components/common/carousel/Carousel.module.scss';
+import Icon from '@/components/common/icon/Icon';
+
+/**
+ *
+ * @param {{items: {id: number | string, item: React.ReactNode}[], swipeCount: number, onClickPrev: () => {}, onClickNext: () => {}}} props
+ * @returns
+ */
+export default function Carousel({
+	itemList,
+	swipeCount = 1,
+	onClickPrev = () => {},
+	onClickNext = () => {},
+}) {
+	const [currentIdx, setCurrentIdx] = useState(0);
+	const swipeCountRef = useRef(swipeCount);
+	const itemMapRef = useRef(null);
+
+	const scrollToId = (itemId) => {
+		const map = getMap();
+		const node = map.get(itemId);
+		node.scrollIntoView({
+			behavior: 'smooth',
+			block: 'nearest',
+			inline: 'start',
+		});
+	};
+
+	const getMap = () => {
+		if (!itemMapRef.current) {
+			itemMapRef.current = new Map();
+		}
+		return itemMapRef.current;
+	};
+
+	const handlePrevClick = () => {
+		scrollToId(itemList.at(currentIdx - swipeCountRef.current).id);
+		setCurrentIdx((ci) => ci - swipeCountRef.current);
+		onClickPrev();
+	};
+
+	const handleNextClick = () => {
+		scrollToId(itemList.at(currentIdx + swipeCountRef.current).id);
+		setCurrentIdx((ci) => ci + swipeCountRef.current);
+		onClickNext();
+	};
+
+	const isFirst = currentIdx === 0;
+	const isEnd = currentIdx + swipeCountRef.current === itemList.length;
+
+	return (
+		<div className={styles.container}>
+			{isFirst ? null : (
+				<button
+					onClick={handlePrevClick}
+					className={clsx(styles.carousel_button, styles.prev)}
+				>
+					<Icon name='arrowLeft' className={styles.left_arrow} />
+				</button>
+			)}
+			{isEnd ? null : (
+				<button
+					onClick={handleNextClick}
+					className={clsx(styles.carousel_button, styles.next)}
+				>
+					<Icon name='arrowRight' className={styles.right_arrow} />
+				</button>
+			)}
+			<ol className={styles.item_list}>
+				{itemList.map(({ id, item }) => (
+					<li
+						key={id}
+						ref={(node) => {
+							const map = getMap();
+							if (node) {
+								map.set(id, node);
+							} else {
+								map.delete(id);
+							}
+						}}
+					>
+						{item}
+					</li>
+				))}
+			</ol>
+		</div>
+	);
+}

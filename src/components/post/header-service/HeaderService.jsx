eba677fa0ec1bec7e1ed10716abf1e0413ffaad6
dkile <dkile532@naver.com>
2023-12-19 20:28:49 +0900

    Fix: 레이아웃 조정 (#74)
    * Fix: 레이아웃 조정
    
    * Fix: 중간 정렬 수정

--- src/components/post/header-service/HeaderService.jsx
+++ src/components/post/header-service/HeaderService.jsx
@@ -3,7 +3,6 @@ import { useMediaQuery } from 'react-responsive';
 import { useLoaderData } from 'react-router-dom';
 
 import { getRecipientsReactions } from '@/apis/recipients/reactionsAPI';
-import Card from '@/components/common/card/Card';
 import Image from '@/components/common/Image';
 import EmojiReactionViewer from '@/components/post/header-service/EmojiReactionViewer';
 import styles from '@/components/post/header-service/HeaderService.module.scss';
@@ -28,40 +27,44 @@ export default function HeaderService() {
 
 	return (
 		<div className={styles.layout}>
-			<h2 className={styles.title}>To.{recipientInfo.name}</h2>
-			<div className={styles.service}>
-				{isPc && (
-					<>
-						<Card.Panel className={styles.sender_profile_container}>
-							<div className={styles.sender_avatar_container}>
-								{take(senderListInfo, MESSAGE_PREVIEW_COUNT).map(
-									(senderList) => (
-										<Image
-											key={senderList.id}
-											src={senderList.profileImageURL}
-											alt={`${senderList.sender} 프로필 이미지`}
-											className={styles.sender_profile}
-										/>
-									),
-								)}
-							</div>
-							{senderListCount - MESSAGE_PREVIEW_COUNT > 0 ? (
-								<div className={styles.more_profile}>
-									+{senderListCount - MESSAGE_PREVIEW_COUNT}
+			<div className={styles.title_container}>
+				<h2 className={styles.title}>To.{recipientInfo.name}</h2>
+			</div>
+			<div className={styles.service_container}>
+				<div className={styles.service}>
+					{isPc && (
+						<>
+							<div className={styles.sender_profile_container}>
+								<div className={styles.sender_avatar_container}>
+									{take(senderListInfo, MESSAGE_PREVIEW_COUNT).map(
+										(senderList) => (
+											<Image
+												key={senderList.id}
+												src={senderList.profileImageURL}
+												alt={`${senderList.sender} 프로필 이미지`}
+												className={styles.sender_profile}
+											/>
+										),
+									)}
 								</div>
-							) : null}
-							<div className={styles.sender_info}>
-								<strong>{senderListCount}</strong>명이 작성했어요!
+								{senderListCount - MESSAGE_PREVIEW_COUNT > 0 ? (
+									<div className={styles.more_profile}>
+										+{senderListCount - MESSAGE_PREVIEW_COUNT}
+									</div>
+								) : null}
+								<div className={styles.sender_info}>
+									<strong>{senderListCount}</strong>명이 작성했어요!
+								</div>
 							</div>
-						</Card.Panel>
-						<div className={styles.divider}></div>
-					</>
-				)}
-				<EmojiReactionViewer />
-				<div className={styles.divider}></div>
-				<ShareButton
-					shareInfo={{ ...recipientInfo, recipientId, reactionCount }}
-				/>
+							<div className={styles.divider}></div>
+						</>
+					)}
+					<EmojiReactionViewer />
+					<div className={styles.divider}></div>
+					<ShareButton
+						shareInfo={{ ...recipientInfo, recipientId, reactionCount }}
+					/>
+				</div>
 			</div>
 		</div>
 	);

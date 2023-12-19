043065cb91e7cb26986727f91780eca1e4b047f3
dkile <dkile532@naver.com>
2023-12-19 20:28:49 +0900

    Fix: 레이아웃 조정 (#74)
    * Fix: 레이아웃 조정
    
    * Fix: 중간 정렬 수정

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,33 +1,5 @@
-import { Link } from 'react-router-dom';
-
-import Icon from '@/components/common/icon/Icon';
 import styles from '@/components/common/Layout.module.scss';
-import { PATH_HOME } from '@/constants/routes';
 
-/**
- *
- * @param {{addon: React.ReactNode, children: React.ReactElement}} props
- */
-export default function Layout({ addon, children }) {
-	return (
-		<div className={styles.container}>
-			<header className={styles.global_header}>
-				<nav className={styles.gnb}>
-					<Link to={PATH_HOME}>
-						<div className={styles.logo_container}>
-							<Icon
-								name='logo'
-								type='img'
-								fetchpriority='high'
-								className={styles.logo}
-							/>
-							<h1>Rolling</h1>
-						</div>
-					</Link>
-					{addon}
-				</nav>
-			</header>
-			<main className={styles.mainContainer}>{children}</main>
-		</div>
-	);
+export default function Layout({ children }) {
+	return <div className={styles.container}>{children}</div>;
 }
--- src/components/list/ListLayout.jsx
+++ src/components/list/ListLayout.jsx
@@ -1,10 +1,23 @@
+import { Link } from 'react-router-dom';
+
+import GlobalHeader from '@/components/common/header/GlobalHeader';
 import Layout from '@/components/common/Layout';
 import styles from '@/components/list/ListLayout.module.scss';
+import { PATH_POST } from '@/constants/routes';
 
 export default function ListLayout({ children }) {
 	return (
 		<Layout>
+			<GlobalHeader addon={<ListHeaderCTA />} />
 			<div className={styles.container}>{children}</div>
 		</Layout>
+	);
+}
+
+function ListHeaderCTA() {
+	return (
+		<Link to={PATH_POST} className={styles.cta}>
+			롤링 페이퍼 만들기
+		</Link>
 	);
 }
